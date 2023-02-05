4665bc19e299a308240b74c67b0b82b0eddb371e
zertfs17 <alfrieghtsnalam@gmail.com>
2023-02-05 03:35:25 +0000

    new update hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
+  public static void main(String[] args) }
+  
+  System.out.println("Hello, World");
+  System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
+ System.out.println("Hello, World");
   }
 }
