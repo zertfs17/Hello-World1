d6c475319534d1a5d2ea95290efcd5e47e3eae39
zertfs17 <alfrieghtsnalam@gmail.com>
2023-02-05 05:45:02 +0000

    new update for  for crontab

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
   System.out.println("Hello, World");
   System.out.println("Hello, World");
  System.out.println("Hello, World");
- System.out.println("Hello, World");
- System.out.println("Hello, World");
+ System.out.println("Im Devops");
+ System.out.println(" Im doing devops learning");
   }
 }
